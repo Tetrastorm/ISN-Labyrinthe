1244b5c0334bfd661a7773aeea3f3916d3940dee
Tetrastorm <Valou91400@gmail.com>
2018-05-08 19:30:21 +0200

    Selection Complete

--- Source/Labyrinthe.py
+++ Source/Labyrinthe.py
@@ -1,7 +1,6 @@
 from tkinter import *
 from math import *
 from random import *
-from time import *
 from Matrix import *
 
 #---------------------------------------------------------------------------------------------------------------
@@ -14,13 +13,16 @@ def TkAfficherMatrice():
     tkCanvas=Canvas(tkFenetre)
     tkCanvas.pack(fill=BOTH, expand=1)
 
+    ObjetMatrice = Matrix(iaMatrice.GetSize())
+
     for y in range(iaMatrice.GetSize()[0]):
         for x in range(iaMatrice.GetSize()[1]):
             if iaMatrice.GetValue([x,y]) == 0:
                 case = tkCanvas.create_rectangle((x * ilImageDimension[0]), (y * ilImageDimension[1]), (x * ilImageDimension[0] + ilImageDimension[0]), (y * ilImageDimension[1] + ilImageDimension[1]), fill="lightgreen")
             elif iaMatrice.GetValue([x, y]) == 1:
                 case = tkCanvas.create_rectangle((x * ilImageDimension[0]), (y * ilImageDimension[1]), (x * ilImageDimension[0] + ilImageDimension[0]), (y * ilImageDimension[1] + ilImageDimension[1]), fill="black")
-    
+            ObjetMatrice.SetMatrix([x,y], case)
+
     cJoueur = tkCanvas.create_rectangle((lCoordJoueur[0] * ilImageDimension[0]), (lCoordJoueur[1] * ilImageDimension[1]), (lCoordJoueur[0] * ilImageDimension[0] + ilImageDimension[0]), (lCoordJoueur[1] * ilImageDimension[1] + ilImageDimension[1]), fill="turquoise")
 
 #---------------------------------------------------------------------------------------------------------------
@@ -68,17 +70,19 @@ def TkEditeur():
     tkEditeurButtonNouveau=Button(tkFenetre, text="Nouvelle Map")
     PositionRelative(tkFenetre, tkEditeurButtonNouveau, [0.80, 0.10])
 
-    TkEditeurButtonPoint = Button(tkFenetre, text="Point", command= lambda:EditeurEvent())
+    TkEditeurButtonPoint = Button(tkFenetre, text="Point", command= lambda:iaMatrice.SetPoint(lCaseCoord[0]))
     PositionRelative(tkFenetre, TkEditeurButtonPoint, [0.80, 0.33])
 
-    TkEditeurButtonLigne = Button(tkFenetre, text="Ligne", command=None)
+    TkEditeurButtonLigne = Button(tkFenetre, text="Ligne", command=lambda:iaMatrice.SetLine(lCaseCoord[0], lCaseCoord[1]))
     PositionRelative(tkFenetre, TkEditeurButtonLigne, [0.85, 0.33])
 
-    TkEditeurButtonRectangle = Button(tkFenetre, text="Rectangle", command=None)
+    TkEditeurButtonRectangle = Button(tkFenetre, text="Rectangle", command=lambda:iaMatrice.SetRectangle(lCaseCoord[0], lCaseCoord[1]))
     PositionRelative(tkFenetre, TkEditeurButtonRectangle, [0.90, 0.33])
 
     TkEditeurButtonMenu = Button(tkFenetre, text="Retourner au menu", command=lambda:TkMenuPrincipale())
     PositionRelative(tkFenetre, TkEditeurButtonMenu, [0.875, 0.90])
+
+    EditeurEvent()
 
     # Créer les widgets de la partie jeu {Statut : En Developpement}
 
@@ -93,7 +97,7 @@ def TkJeu():
 #---------------------------------------------------------------------------------------------------------------
 
 def EditeurEvent():
-    tkFenetre.bind('<Button-1>', GetMouseCoord)
+    tkFenetre.bind('<Button-1>', GetCase)
 
 #---------------------------------------------------------------------------------------------------------------
 #                                                 Jeu
@@ -138,12 +142,13 @@ def EnleverWidget(tkFenetre):
     for item in lWidget:
         item.destroy()
 
-def GetMouseCoord(event):
-    lMouseCoord[1] = lMouseCoord[0]
-    lMouseCoord[0][0]=tkFenetre.winfo_pointerx()
-    lMouseCoord[0][1]=tkFenetre.winfo_pointery()
+def GetCase(event):
+    lCaseCoord[1][0] = lCaseCoord[0][0]
+    lCaseCoord[1][1] = lCaseCoord[0][1]
+    lCaseCoord[0][0]= int(tkFenetre.winfo_pointerx()/(2*ilImageDimension[0])) 
+    lCaseCoord[0][1]= int(tkFenetre.winfo_pointery()/(2*ilImageDimension[1]))
 
-    print(str(lMouseCoord))
+    print(str(lCaseCoord))
 
 #---------------------------------------------------------------------------------------------------------------
 #                                           Le Laboratoire
@@ -194,12 +199,12 @@ def AI_Perception(ActorInfo, OtherActorInfo, Range = 5):
 #                                             Programme principale
 #---------------------------------------------------------------------------------------------------------------
 
-global ilImageDimension, lCoordJoueur, cJoueur, iaMatrice, tkCanvas, lMouseCoord
+global ilImageDimension, lCoordJoueur, cJoueur, iaMatrice, tkCanvas, lCaseCoord
 
 lTailleMatrice = [30, 30]
 lCoordJoueur=[0,0, 1]   #[0] coord X, [1] coord Y, [2] Orientation (0 = Up, 1 = Right, 2 = Down, 3 = Left)
 
-lMouseCoord = [[0,0], [0,0]]
+lCaseCoord = [[0,0], [0,0]]
 
 ilImageDimension = [15, 15]
 
